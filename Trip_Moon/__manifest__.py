eb6aacc6842f40689e156890a35cabbfbfd0f514
aosoriod <aosoriod@gmail.com>
2022-09-18 15:38:39 +0000

    [ADD] Module Basic

---
+++ Trip_Moon/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Trip_Moon",
+    "summary": """Travel moon""",
+    "description": """
+        Travel to the moon:
+        -Planning
+        -help
+    """,
+    "autor": "NovaPos",
+    "website": "https://ww.novaPos.com",
+    "category": "Training",
+    "version": "0,1",
+    "depends": ["base"],
+    "data": [
+        
+    ],
+    "demo":[
+        
+    ],
+    
+}
